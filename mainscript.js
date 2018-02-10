4d26b985ecea403285f96e7a32344e38af6af042
Elton C. Rego <rego.elton@gmail.com>
2018-02-10 12:01:02 -0800

    Mid way check

---
+++ mainscript.js
@@ -0,0 +1,20 @@
+var newContact = {
+  // firstName: "Contact",
+  // lastName: "Name",
+  // emailAddress: "test@test.testy",
+  // messageBody: "'Hello there' - Obi Wan Kenobi",
+  // optionUpdates: true,
+  // optionServices: false,
+}
+
+// submitForm -> name -> function() {}
+var submitForm = function(){
+  // User checking -> make sure user isn't stupid
+  // Check if the message is full
+  // Assign each field to an object
+  // Actually submit
+}
+
+var userCheck = function(){
+  
+}
